58362bb15778534b04c341f42ce35b47d1b03fbd
kukuh-biyantama <kukuhbiyantama2244@gmail.com>
2022-02-26 16:33:27 +0700

    Latihan Mengirim Data dengan Parcelable

---
+++ app/src/main/java/com/dicoding/picodiploma/dataforparcelable/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.dicoding.picodiploma.dataforparcelable
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //memperkenalkan tombol
+        val btnMoveWithObjectActivity: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObjectActivity.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                        "DicodingAcademy",
+                        5,
+                        "academy@dicoding.com",
+                        "Bandung"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/dataforparcelable/MoveWithObjectActivity.kt
@@ -0,0 +1,25 @@
+package com.dicoding.picodiploma.dataforparcelable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWithObjectActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_PERSON = "extra_person"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_object)
+
+        //inisialisasi textview
+        val tvObject: TextView = findViewById(R.id.tv_object_received)
+
+        //menampung data
+        val person = intent.getParcelableExtra<Person>(EXTRA_PERSON) as Person
+        val text = "Name : ${person.name.toString()},\nEmail : ${person.email},\nAge : ${person.age},\nLocation : ${person.city}"
+        tvObject.text = text
+    }
+}
